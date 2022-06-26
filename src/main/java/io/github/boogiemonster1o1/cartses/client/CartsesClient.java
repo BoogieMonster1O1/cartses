86b5bd7e090b2dbfdde06523380def6848fa7a78
SD <harrypotter.starwars.shrish@gmail.com>
2022-06-26 18:54:03 +0530

    Register

--- src/main/java/io/github/boogiemonster1o1/cartses/client/CartsesClient.java
+++ src/main/java/io/github/boogiemonster1o1/cartses/client/CartsesClient.java
@@ -1,9 +1,11 @@
 package io.github.boogiemonster1o1.cartses.client;
 
+import io.github.boogiemonster1o1.cartses.entity.CartsesEntities;
 import io.github.boogiemonster1o1.cartses.entity.CartsesEntityType;
 
 import net.minecraft.client.render.entity.model.EntityModelLayer;
 import net.minecraft.client.render.entity.model.MinecartEntityModel;
+import net.minecraft.util.Util;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
@@ -13,12 +15,17 @@ public class CartsesClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		CartsesEntityType.ALL.forEach(type -> {
-			EntityModelLayer layer = new EntityModelLayer(type.getId(), "main");
-			EntityModelLayerRegistry.registerModelLayer(layer, MinecartEntityModel::getTexturedModelData);
 			if (type.noRenderer()) {
 				return;
 			}
+			EntityModelLayer layer = new EntityModelLayer(type.getId(), "main");
+			EntityModelLayerRegistry.registerModelLayer(layer, MinecartEntityModel::getTexturedModelData);
 			EntityRendererRegistry.register(type, ctx -> new EmissiveMinecartRenderer<>(ctx, layer));
 		});
+		EntityRendererRegistry.register(CartsesEntities.SHULKER_BOX_MINECART, ctx -> new ShulkerBoxMinecartRenderer(ctx, Util.make(() -> {
+			EntityModelLayer layer = new EntityModelLayer(CartsesEntities.SHULKER_BOX_MINECART.getId(), "main");
+			EntityModelLayerRegistry.registerModelLayer(layer, MinecartEntityModel::getTexturedModelData);
+			return layer;
+		})));
 	}
 }
